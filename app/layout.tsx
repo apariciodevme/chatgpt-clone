b69a503392b1d1cd7560b620339b304f8723b7e2
apariciodevme <apariciodev@proton.me>
2023-06-27 12:29:58 -0300

    user image, signout function

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import Login from "@/components/Login";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -20,27 +19,26 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
 
-  const session = await getServerSession(authOptions)
   return (
     <html lang="en">
       <body className={inter.className}>
-      <SessionProvider session={session}>
-        {!session ? (
-          <Login/>
-        ) : (
-          <div className="flex">
-          {/*Side Bar*/}
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]  ">
-            <SideBar />
-          </div>
-          {/*Client Provider - Notifications*/}
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              {/*Side Bar*/}
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]  ">
+                <SideBar />
+              </div>
+              {/*Client Provider - Notifications*/}
 
-          <div className="flex-1 bg-[#343541]">{children}</div>
-        </div>
-        )}
-      </SessionProvider>
-       
+              <div className="flex-1 bg-[#343541]">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,18 +1,32 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
 
 export const SideBar = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col min-h-screen p-2">
       <div className="flex-1">
         <div>
           {/*New chat button */}
-          <NewChat/>
+          <NewChat />
 
           <div>{/*Model selection component*/}</div>
 
           {/*map chat rows*/}
         </div>
       </div>
+
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          className="w-12 h-12 mx-auto mb-2 rounded-full cursor-pointer hover:opacity-50"
+          alt="user image"
+        />
+      )}
     </div>
   );
 };

8be494edeb1b19b8e0f691440d9a55507363d5e2
apariciodevme <apariciodev@proton.me>
2023-06-27 01:38:19 -0300

    nextauth, firebase set up

---
+++ api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.FIREBASE_ID!,
+      clientSecret: process.env.FIREBASE_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+}
+
+export default NextAuth(authOptions)
